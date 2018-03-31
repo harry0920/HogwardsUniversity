d0e5ba92b5ae7938f561ef23e73e52c10e9656a2
hardikv <vas13008@byui.edu>
2018-03-31 16:40:57 -0600

    Add project files.

---
+++ HogwardsUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HogwardsUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
